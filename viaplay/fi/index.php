30885af771cc5830889612118b63dec982ea93c0
Johan Caripson <31686838+Caripson@users.noreply.github.com>
2018-06-29 23:25:26 +0200

    Fixed time issue

--- viaplay/fi/index.php
+++ viaplay/fi/index.php
@@ -3,12 +3,12 @@
 header('Content-type: text/calendar; charset=utf-8');
 header('Content-Disposition: inline; filename=index.ics');
 
-$url = 'https://content.viaplay.fi/pcdash-fi/urheilu'; 
-$data = file_get_contents($url); 
-$objOrArray = json_decode($data, true); 
+$url = 'https://content.viaplay.fi/pcdash-fi/urheilu';
+$data = file_get_contents($url);
+$objOrArray = json_decode($data, true);
 
 
-	
+
 function traverse(&$objOrArray)
 {
 
@@ -48,29 +48,29 @@ function traverse(&$objOrArray)
 	{
 		if ($key == "synopsis"){
 		  $synopsis_status=true;
-		}	
+		}
 
 	if (is_array($value) || is_object($value))
-		{	
-		
+		{
+
 
 			traverse($value);
 		}
 	else
 	{
 		  $skipThis = array("Serie A","NFL Network","Various","sport-main","hd","sd","sport-live","notInImdb","portrait","Unknown","featurebox","Visa hela tablån","landscape","sport-schedule-per-day");
-		  
+
 		  if (in_array($value, $skipThis)) {
 
-		  }else{	  
+		  }else{
 
   		if ($synopsis_status == true)
 		{
 
 			if ($key == "title"){
-				$title_status=true;	
+				$title_status=true;
 				$title=$value;
-			
+
 			}
 
 			if ($key == "synopsis"){
@@ -80,7 +80,7 @@ function traverse(&$objOrArray)
 			}
 
 			if ($key == "streamStart"){
-			
+
 			$streamStart = $value;
 
                         if (holds_int($value)){
@@ -96,17 +96,17 @@ function traverse(&$objOrArray)
 			$streamEnd = $value;
 			$streamEnd_status = true;
 
-			$checkIFok = array($title_status, $synopsis_status, $streamStart_status, $streamEnd_status); 
+			$checkIFok = array($title_status, $synopsis_status, $streamStart_status, $streamEnd_status);
 			$checkIFok_status = false;
-			
+
 			global $checkIFok_status;
 
 			foreach ($checkIFok as $value) {
-				
+
 			if($value)
 			{
 				$checkIFok_status = true;
-											
+
 			}else{
 				$checkIFok_status = false;
 			}
@@ -114,11 +114,11 @@ function traverse(&$objOrArray)
 
 			if ($checkIFok_status)
 			{
-		
+
 			$streamStart = str_replace(':', '', $streamStart);
                         $streamStart = str_replace('-', '', $streamStart);
 	        	$streamStart = str_replace('.', '', $streamStart);
-			
+
 			$streamEnd = str_replace(':', '', $streamEnd);
 			$streamEnd = str_replace('-', '', $streamEnd);
 			$streamEnd = str_replace('.', '', $streamEnd);
@@ -133,30 +133,30 @@ function traverse(&$objOrArray)
 
 
 			echo("BEGIN:VEVENT" . "\r\n");
-			echo("DTSTART:" . $streamStart . "\r\n" );
-			echo("DTEND:" . $streamEnd  . "\r\n");
-			echo("DTSTAMP:" . $streamStart . "\r\n");
-			echo("CREATED:" . $streamStart . "\r\n");
+			echo("DTSTART:" . $streamStart . "Z\r\n" );
+			echo("DTEND:" . $streamEnd  . "Z\r\n");
+			echo("DTSTAMP:" . $streamStart . "Z\r\n");
+			echo("CREATED:" . $streamStart . "Z\r\n");
 			echo("DESCRIPTION:" .  $synopsis  . "\r\n");
-			echo("LAST-MODIFIED:". $streamStart . "\r\n");
+			echo("LAST-MODIFIED:". $streamStart . "Z\r\n");
 			echo("LOCATION:" . "\r\n");
 			echo("SEQUENCE:0" . "\r\n");
 			echo("STATUS:CONFIRMED" . "\r\n");
 			echo("SUMMARY:" . $title  . "\r\n");
 			echo("UID:" . guidv4(random_bytes(16))  . "\r\n");
 			echo("TRANSP:OPAQUE" . "\r\n");
-			echo("END:VEVENT" . "\r\n");	
+			echo("END:VEVENT" . "\r\n");
 
 			$checkIFok_status = false;
-			
+
 			$synopsis=false;
                         $title_status = false;
                         $synopsis_status = false;
                         $streamStart_status = false;
                         $streamEnd_status = false;
-							    
+
 			}
-										
+
 			}
 		}
 	}
@@ -171,16 +171,16 @@ echo("X-WR-CALNAME:Viaplay Sporti Finland"."\r\n");
 echo("X-WR-TIMEZONE:Europe/Helsinki"."\r\n");
 echo("X-WR-CALDESC:Viaplay Calender -tiedot osoitteesta www.viaplay.fi"."\r\n");
 echo("X-PUBLISHED-TTL:PT15M"."\r\n");
-traverse($objOrArray);	
-echo("END:VCALENDAR"); 
+traverse($objOrArray);
+echo("END:VCALENDAR");
 
 
 function guidv4($data)
 {
     assert(strlen($data) == 16);
 
-    $data[6] = chr(ord($data[6]) & 0x0f | 0x40); 
-    $data[8] = chr(ord($data[8]) & 0x3f | 0x80); 
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
 
     return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
 }
@@ -198,4 +198,3 @@ else
 
 
 ?>
-
--- viaplay/no/index.php
+++ viaplay/no/index.php
@@ -3,12 +3,12 @@
 header('Content-type: text/calendar; charset=utf-8');
 header('Content-Disposition: inline; filename=index.ics');
 
-$url = 'https://content.viaplay.no/pcdash-no/sport'; 
-$data = file_get_contents($url); 
-$objOrArray = json_decode($data, true); 
+$url = 'https://content.viaplay.no/pcdash-no/sport';
+$data = file_get_contents($url);
+$objOrArray = json_decode($data, true);
 
 
-	
+
 function traverse(&$objOrArray)
 {
 
@@ -48,29 +48,29 @@ function traverse(&$objOrArray)
 	{
 		if ($key == "synopsis"){
 		  $synopsis_status=true;
-		}	
+		}
 
 	if (is_array($value) || is_object($value))
-		{	
-		
+		{
+
 
 			traverse($value);
 		}
 	else
 	{
 		  $skipThis = array("Serie A","NFL Network","Various","sport-main","hd","sd","sport-live","notInImdb","portrait","Unknown","featurebox","Visa hela tablån","landscape","sport-schedule-per-day");
-		  
+
 		  if (in_array($value, $skipThis)) {
 
-		  }else{	  
+		  }else{
 
   		if ($synopsis_status == true)
 		{
 
 			if ($key == "title"){
-				$title_status=true;	
+				$title_status=true;
 				$title=$value;
-			
+
 			}
 
 			if ($key == "synopsis"){
@@ -81,7 +81,7 @@ function traverse(&$objOrArray)
 
 			if ($key == "streamStart"){
 			$streamStart = $value;
-			
+
 			if (holds_int($value)){
                         $streamStart_status = true;
 			}else{
@@ -95,17 +95,17 @@ function traverse(&$objOrArray)
 			$streamEnd = $value;
 			$streamEnd_status = true;
 
-			$checkIFok = array($title_status, $synopsis_status, $streamStart_status, $streamEnd_status); 
+			$checkIFok = array($title_status, $synopsis_status, $streamStart_status, $streamEnd_status);
 			$checkIFok_status = false;
-			
+
 			global $checkIFok_status;
 
 			foreach ($checkIFok as $value) {
-				
+
 			if($value)
 			{
 				$checkIFok_status = true;
-											
+
 			}else{
 				$checkIFok_status = false;
 			}
@@ -113,15 +113,15 @@ function traverse(&$objOrArray)
 
 			if ($checkIFok_status)
 			{
-		
+
 			$streamStart = str_replace(':', '', $streamStart);
                         $streamStart = str_replace('-', '', $streamStart);
 	        	$streamStart = str_replace('.', '', $streamStart);
-			
+
 			$streamEnd = str_replace(':', '', $streamEnd);
 			$streamEnd = str_replace('-', '', $streamEnd);
 			$streamEnd = str_replace('.', '', $streamEnd);
-			
+
 
 
                         $streamStart =  str_split($streamStart,15)[0];
@@ -131,31 +131,31 @@ function traverse(&$objOrArray)
                         $title=  preg_replace( "/\r|\n/", "", $title );
 
 			echo("BEGIN:VEVENT" . "\r\n");
-			echo("DTSTART:" . $streamStart . "\r\n" );
-			echo("DTEND:" . $streamEnd  . "\r\n");
-			echo("DTSTAMP:" . $streamStart . "\r\n");
-			echo("CREATED:" . $streamStart . "\r\n");
+			echo("DTSTART:" . $streamStart . "Z\r\n" );
+			echo("DTEND:" . $streamEnd  . "Z\r\n");
+			echo("DTSTAMP:" . $streamStart . "Z\r\n");
+			echo("CREATED:" . $streamStart . "Z\r\n");
 			echo("DESCRIPTION:" .  $synopsis  . "\r\n");
-			echo("LAST-MODIFIED:". $streamStart . "\r\n");
+			echo("LAST-MODIFIED:". $streamStart . "Z\r\n");
 			echo("LOCATION:" . "\r\n");
 			echo("SEQUENCE:0" . "\r\n");
 			echo("STATUS:CONFIRMED" . "\r\n");
 			echo("SUMMARY:" . $title  . "\r\n");
 			echo("UID:" . guidv4(random_bytes(16))  . "\r\n");
 			echo("TRANSP:OPAQUE" . "\r\n");
-			echo("END:VEVENT" . "\r\n");	
+			echo("END:VEVENT" . "\r\n");
 
 			$checkIFok_status = false;
-			
+
 			$synopsis=false;
                         $title_status = false;
                         $synopsis_status = false;
                         $streamStart_status = false;
                         $streamEnd_status = false;
-			
-					    
+
+
 			}
-										
+
 			}
 		}
 	}
@@ -170,16 +170,16 @@ echo("X-WR-CALNAME:Viaplay Sport Norway"."\r\n");
 echo("X-WR-TIMEZONE:Europe/Oslo"."\r\n");
 echo("X-WR-CALDESC:Viaplay Calender data from www.viaplay.no"."\r\n");
 echo("X-PUBLISHED-TTL:PT15M"."\r\n");
-traverse($objOrArray);	
-echo("END:VCALENDAR"); 
+traverse($objOrArray);
+echo("END:VCALENDAR");
 
 
 function guidv4($data)
 {
     assert(strlen($data) == 16);
 
-    $data[6] = chr(ord($data[6]) & 0x0f | 0x40); 
-    $data[8] = chr(ord($data[8]) & 0x3f | 0x80); 
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
 
     return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
 }
@@ -188,7 +188,7 @@ function holds_int($str)
 {
 if (preg_match('/[A-Za-z]/', $str) && preg_match('/[0-9]/', $str))
 {
-		return false;	
+		return false;
 }
 else
 {
@@ -198,4 +198,3 @@ else
 
 
 ?>
-
